8516a9886ac1411220fd7ff6c68857241603f486
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-13 23:57:12 +0700

    Create response.py

---
+++ response.py
@@ -0,0 +1,8 @@
+response_a = "If the client failed to authenticate, press ar in the console"
+response_b = "If the game is crashing during loading screens, verify integrity"
+response_c = "Try playng on discord if the game is crashing"
+response_d = "The menu has lots to offer!"
+blank_spot = "food"
+
+
+responses = [response_a, response_b, response_c, response_d]
