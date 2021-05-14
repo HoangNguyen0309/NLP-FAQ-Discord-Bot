1a431de4127afa58f4de64dd8006de47df55bf43
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-14 16:32:34 +0700

    Update response.py

--- response.py
+++ response.py
@@ -1,8 +1,10 @@
-response_a = "If the client failed to authenticate, press ar in the console"
-response_b = "If the game is crashing during loading screens, verify integrity"
-response_c = "Try playng on discord if the game is crashing"
-response_d = "The menu has lots to offer!"
+response_a = "If you are banned, tech support cannot help you"
+response_b = "If the game is crashing during loading screens, verify integrity or make sure the game is up to minimum requirements"
+response_c = "If the game is laggy, lower settings or make sure the game is up to minimum requirements"
+response_d = "Tech support cannot help you with a game ban"
+response_f = "If the client failed to authenticate, press ar in the console"
+response_e = "If connection is lost, check your internet"
 blank_spot = "food"
 
 
-responses = [response_a, response_b, response_c, response_d]
+responses = [response_a, response_b, response_c, response_d, response_e, response_f]
